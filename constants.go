b1bd4fb11a9680df74b235488966d3905da1ecf6
Ian A Mason <iam@csl.sri.com>
2017-06-30 13:25:05 -0700

    Keep everything happy.

--- constants.go
+++ constants.go
@@ -6,7 +6,7 @@ import (
 
 const (
 	//The name of our ELF section of "bitcode paths".
-	ELFSectionName    = ".llvm_bc"
+	ELFSectionName = ".llvm_bc"
 	//The name of our MACH-O segment of "bitcode paths".
 	DarwinSegmentName = "__WLLVM"
 	//The name of our MACH-O section of "bitcode paths".
@@ -15,12 +15,16 @@ const (
 
 //The user configured directory holding the LLVM binary tools.
 var LLVMToolChainBinDir string
+
 //The user configured name of the clang compiler.
 var LLVMCCName string
+
 //The user configured name of the clang++ compiler.
 var LLVMCXXName string
+
 //The user configured name of the llvm-ar.
 var LLVMARName string
+
 //The user configured name of the llvm-link.
 var LLVMLINKName string
 
