675315528963fb77e1fd9b27e5b31690fa01ad7f
Ian A. Mason <iam@csl.sri.com>
2017-06-29 18:07:31 -0700

    golinting [ci skip]

--- constants.go
+++ constants.go
@@ -2,12 +2,12 @@ package main
 
 const (
 	// Environment variables
-	envCONFIGUREONLY    = "GLLVM_CONFIGURE_ONLY"
-	envTOOLSPATH        = "GLLVM_TOOLS_PATH"
+	envCONFIGUREONLY   = "GLLVM_CONFIGURE_ONLY"
+	envTOOLSPATH       = "GLLVM_TOOLS_PATH"
 	envCCOMPILERNAME   = "GLLVM_CC_NAME"
 	envCXXCOMPILERNAME = "GLLVM_CXX_NAME"
-	envLINKERNAME       = "GLLVM_LINK_NAME"
-	envARNAME           = "GLLVM_AR_NAME"
+	envLINKERNAME      = "GLLVM_LINK_NAME"
+	envARNAME          = "GLLVM_AR_NAME"
 	envBCSTOREPATH     = "GLLVM_BC_STORE"
 
 	// Gllvm functioning  (once we have it working we can change the W to G; but for the time being leave it so that extract-bc works)
