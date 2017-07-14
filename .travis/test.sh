51720e1461041d3ab289b38e9217ddd7d50330a5
Ian A. Mason <iam@csl.sri.com>
2017-07-14 10:55:17 -0700

    fix travis to use the new old environment variables.

--- .travis/test.sh
+++ .travis/test.sh
@@ -4,14 +4,14 @@ set -e
 
 
 export PATH=/usr/lib/llvm-3.8/bin:${PATH}
-export GLLVM_OUTPUT_LEVEL=WARNING
+export WLLVM_OUTPUT_LEVEL=WARNING
 
 
 git clone https://github.com/SRI-CSL/musllvm.git musllvm
 
 cd musllvm
 
-GLLVM_CONFIGURE_ONLY=1  CC=gclang ./configure --target=LLVM --build=LLVM
+WLLVM_CONFIGURE_ONLY=1  CC=gclang ./configure --target=LLVM --build=LLVM
 
 make
 
