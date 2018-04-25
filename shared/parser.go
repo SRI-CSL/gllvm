142f82df383285415bd9da5c89177395d800f34e
Ian A. Mason <iam@csl.sri.com>
2018-04-25 14:05:52 -0700

    gllvm and wllvm agree when compiling the linux kernel.

--- shared/parser.go
+++ shared/parser.go
@@ -121,50 +121,50 @@ func parse(argList []string) parserResult {
 		"-nostdlibinc": {0, pr.compileUnaryCallback},
 
 		"-mno-omit-leaf-frame-pointer": {0, pr.compileUnaryCallback},
-		"-maes":           {0, pr.compileUnaryCallback},
-		"-mno-aes":        {0, pr.compileUnaryCallback},
-		"-mavx":           {0, pr.compileUnaryCallback},
-		"-mno-avx":        {0, pr.compileUnaryCallback},
-		"-mavx2":          {0, pr.compileUnaryCallback},
-		"-mno-avx2":       {0, pr.compileUnaryCallback},
-		"-mcmodel=kernel": {0, pr.compileUnaryCallback},
-		"-mno-red-zone":   {0, pr.compileUnaryCallback},
-		"-mmmx":           {0, pr.compileUnaryCallback},
-		"-mno-mmx":        {0, pr.compileUnaryCallback},
-		"-mno-global-merge":     {0, pr.compileUnaryCallback},   //iam: linux kernel stuff
-		"-mno-80387":     {0, pr.compileUnaryCallback},   //iam: linux kernel stuff
-		"-msse":           {0, pr.compileUnaryCallback},
-		"-mno-sse":        {0, pr.compileUnaryCallback},
-		"-msse2":          {0, pr.compileUnaryCallback},
-		"-mno-sse2":       {0, pr.compileUnaryCallback},
-		"-msse3":          {0, pr.compileUnaryCallback},
-		"-mno-sse3":       {0, pr.compileUnaryCallback},
-		"-mssse3":         {0, pr.compileUnaryCallback},
-		"-mno-ssse3":      {0, pr.compileUnaryCallback},
-		"-msse4":          {0, pr.compileUnaryCallback},
-		"-mno-sse4":       {0, pr.compileUnaryCallback},
-		"-msse4.1":        {0, pr.compileUnaryCallback},
-		"-mno-sse4.1":     {0, pr.compileUnaryCallback},
-		"-msse4.2":        {0, pr.compileUnaryCallback},
-		"-mno-sse4.2":     {0, pr.compileUnaryCallback},
-		"-msoft-float":    {0, pr.compileUnaryCallback},
-		"-m3dnow":         {0, pr.compileUnaryCallback},
-		"-mno-3dnow":      {0, pr.compileUnaryCallback},
-		"-m16":            {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
-		"-m32":            {0, pr.compileUnaryCallback},
-		"-m64":            {0, pr.compileUnaryCallback},
-		"-mstackrealign":  {0, pr.compileUnaryCallback},
-		"-mretpoline-external-thunk": {0, pr.compileUnaryCallback},   //iam: linux kernel stuff
-		"-mno-fp-ret-in-387":  {0, pr.compileUnaryCallback},          //iam: linux kernel stuff
-		"-mskip-rax-setup":  {0, pr.compileUnaryCallback},            //iam: linux kernel stuff
-		"-mindirect-branch-register":  {0, pr.compileUnaryCallback},  //iam: linux kernel stuff
+		"-maes":                      {0, pr.compileUnaryCallback},
+		"-mno-aes":                   {0, pr.compileUnaryCallback},
+		"-mavx":                      {0, pr.compileUnaryCallback},
+		"-mno-avx":                   {0, pr.compileUnaryCallback},
+		"-mavx2":                     {0, pr.compileUnaryCallback},
+		"-mno-avx2":                  {0, pr.compileUnaryCallback},
+		"-mcmodel=kernel":            {0, pr.compileUnaryCallback},
+		"-mno-red-zone":              {0, pr.compileUnaryCallback},
+		"-mmmx":                      {0, pr.compileUnaryCallback},
+		"-mno-mmx":                   {0, pr.compileUnaryCallback},
+		"-mno-global-merge":          {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
+		"-mno-80387":                 {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
+		"-msse":                      {0, pr.compileUnaryCallback},
+		"-mno-sse":                   {0, pr.compileUnaryCallback},
+		"-msse2":                     {0, pr.compileUnaryCallback},
+		"-mno-sse2":                  {0, pr.compileUnaryCallback},
+		"-msse3":                     {0, pr.compileUnaryCallback},
+		"-mno-sse3":                  {0, pr.compileUnaryCallback},
+		"-mssse3":                    {0, pr.compileUnaryCallback},
+		"-mno-ssse3":                 {0, pr.compileUnaryCallback},
+		"-msse4":                     {0, pr.compileUnaryCallback},
+		"-mno-sse4":                  {0, pr.compileUnaryCallback},
+		"-msse4.1":                   {0, pr.compileUnaryCallback},
+		"-mno-sse4.1":                {0, pr.compileUnaryCallback},
+		"-msse4.2":                   {0, pr.compileUnaryCallback},
+		"-mno-sse4.2":                {0, pr.compileUnaryCallback},
+		"-msoft-float":               {0, pr.compileUnaryCallback},
+		"-m3dnow":                    {0, pr.compileUnaryCallback},
+		"-mno-3dnow":                 {0, pr.compileUnaryCallback},
+		"-m16":                       {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
+		"-m32":                       {0, pr.compileUnaryCallback},
+		"-m64":                       {0, pr.compileUnaryCallback},
+		"-mstackrealign":             {0, pr.compileUnaryCallback},
+		"-mretpoline-external-thunk": {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
+		"-mno-fp-ret-in-387":         {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
+		"-mskip-rax-setup":           {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
+		"-mindirect-branch-register": {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
 
 		"-A": {1, pr.compileBinaryCallback},
 		"-D": {1, pr.compileBinaryCallback},
 		"-U": {1, pr.compileBinaryCallback},
 
-		"-P": {1, pr.compileUnaryCallback},     //iam: linux kernel stuff (linker script stuff)
-		"-C": {1, pr.compileUnaryCallback},     //iam: linux kernel stuff (linker script stuff)
+		"-P": {1, pr.compileUnaryCallback}, //iam: linux kernel stuff (linker script stuff)
+		"-C": {1, pr.compileUnaryCallback}, //iam: linux kernel stuff (linker script stuff)
 
 		"-M":   {0, pr.dependencyOnlyCallback},
 		"-MM":  {0, pr.dependencyOnlyCallback},
@@ -211,7 +211,7 @@ func parse(argList []string) parserResult {
 		"-Os":    {0, pr.compileUnaryCallback},
 		"-Ofast": {0, pr.compileUnaryCallback},
 		"-Og":    {0, pr.compileUnaryCallback},
-		"-Oz":    {0, pr.compileUnaryCallback},  //iam: linux kernel
+		"-Oz":    {0, pr.compileUnaryCallback}, //iam: linux kernel
 
 		"-Xclang":        {1, pr.compileBinaryCallback},
 		"-Xpreprocessor": {1, pr.defaultBinaryCallback},
@@ -268,13 +268,13 @@ func parse(argList []string) parserResult {
 		`^--sysroot=.+$`:                        {0, pr.compileUnaryCallback},
 		`^-print-prog-name=.*$`:                 {0, pr.compileUnaryCallback},
 		`^-print-file-name=.*$`:                 {0, pr.compileUnaryCallback},
-		`^-mstack-alignment=.+$`:                {0, pr.compileUnaryCallback},  //iam: linux kernel stuff
-		`^-march=.+$`:                           {0, pr.compileUnaryCallback},  //iam: linux kernel stuff
-		`^-mregparm=.+$`:                        {0, pr.compileUnaryCallback},  //iam: linux kernel stuff
-		`^-mcmodel=.+$`:                         {0, pr.compileUnaryCallback},  //iam: linux kernel stuff
-		`^-mpreferred-stack-boundary=.+$`:       {0, pr.compileUnaryCallback},  //iam: linux kernel stuff
-		`^-mindirect-branch=.+$`:                {0, pr.compileUnaryCallback},  //iam: linux kernel stuff
-		`^--param=.+$` :                         {0, pr.compileUnaryCallback},  //iam: linux kernel stuff
+		`^-mstack-alignment=.+$`:                {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
+		`^-march=.+$`:                           {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
+		`^-mregparm=.+$`:                        {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
+		`^-mcmodel=.+$`:                         {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
+		`^-mpreferred-stack-boundary=.+$`:       {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
+		`^-mindirect-branch=.+$`:                {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
+		`^--param=.+$`:                          {0, pr.compileUnaryCallback}, //iam: linux kernel stuff
 
 	}
 
