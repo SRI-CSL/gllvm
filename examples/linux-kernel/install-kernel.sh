095b950cfd735cbeba6d94265d88bbaf0ea552cf
paul-naert <paul_naert@hotmail.fr>
2018-05-22 17:20:00 -0700

    Corrected wrong folder name

--- examples/linux-kernel/install-kernel.sh
+++ examples/linux-kernel/install-kernel.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 ### Copy vmlinux into the bootable linux folder and install the new kernel
-cp $HOME/standalone-build/vmlinux $HOME/bootable-kernel/
+cp $HOME/standalone-build/vmlinux $HOME/bootable-linux/
 
-cd $HOME/bootable-kernel
+cd $HOME/bootable-linux
 
 scripts/sortextable vmlinux 
 nm -n vmlinux | grep -v '\( [aNUw] \)\|\(__crc_\)\|\( \$[adt]\)\|\( .L\)' > System.map
