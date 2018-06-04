4a25691b8d662d71c97724f7daa102c0552588c9
Ian A. Mason <iam@csl.sri.com>
2018-06-04 13:37:03 -0700

    multi-os travis CI

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,29 +2,13 @@
 # Make sure we exit if there is a failure
 set -e
 
+cd .travis
+
+echo `pwd`
 
 export PATH=/usr/lib/llvm-3.8/bin:${PATH}
 export WLLVM_OUTPUT_LEVEL=WARNING
 
-
-git clone https://github.com/SRI-CSL/musllvm.git musllvm
-
-cd musllvm
-
-WLLVM_CONFIGURE_ONLY=1  CC=gclang ./configure --target=LLVM --build=LLVM
-
 make
 
 exit $?
-
-
-get-bc -b ./lib/libc.a
-
-if [ -s "./lib/libc.a.bc" ]
-then
-    echo "libc.a.bc exists."
-else
-    exit 1
-fi
-
-exit 0
