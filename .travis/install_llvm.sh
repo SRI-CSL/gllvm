44abca07193ed50c99ca40a2c0196580989822a5
Ian A. Mason <iam@csl.sri.com>
2018-06-04 13:41:21 -0700

    multi-os travis CI (oopsy forgot a script)

---
+++ .travis/install_llvm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+set -ev
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    wget http://csl.sri.com/users/iam/llvm_lite-6.0.0.high_sierra.bottle.1.tar.gz
+    brew install -v ./llvm_lite-6.0.0.high_sierra.bottle.1.tar.gz
+fi
+
