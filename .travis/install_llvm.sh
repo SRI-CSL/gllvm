36f9b67dbf8040e6691ca7f734a7210dbcfc960c
Ian A.Mason <iam@csl.sri.com>
2020-07-13 13:28:22 -0700

    CI wild goose chase, II

--- .travis/install_llvm.sh
+++ .travis/install_llvm.sh
@@ -2,7 +2,7 @@
 set -ev
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew install -v llvm
-    which llvm-link
-    exit(0)
+    wget http://csl.sri.com/users/iam/llvm_lite-6.0.0.high_sierra.bottle.1.tar.gz
+    brew install -v ./llvm_lite-6.0.0.high_sierra.bottle.1.tar.gz
+    #brew install -v llvm
 fi
