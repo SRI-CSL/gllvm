d235e663df35a651d8f13aef0cb762da252376ca
William Woodruff <william@trailofbits.com>
2020-08-04 10:22:40 -0400

    get-bc: exit with Extract()'s exit code

--- cmd/get-bc/main.go
+++ cmd/get-bc/main.go
@@ -42,11 +42,9 @@ func main() {
 	// Parse command line
 	var args = os.Args
 
-	shared.Extract(args)
+	exitCode := shared.Extract(args)
 
 	shared.LogInfo("Calling %v DID NOT TELL US WHAT HAPPENED\n", os.Args)
 
-	// could be more honest about our success here
-	os.Exit(0)
-
+	os.Exit(exitCode)
 }
